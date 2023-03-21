0b7fdaf0e68675a20561eea16c36a5f1273b8103
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-03-21 10:47:28 +0200

    add rendermarkup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import API from './fetchPictures';
+import Notiflix from 'notiflix';
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
@@ -10,24 +11,53 @@ refs.searchForm.addEventListener('submit', onSearch);
 function onSearch(e) {
     e.preventDefault();
     let textInput = e.target.searchQuery.value;
-    // console.log(textInput)
-    // resetMarkup();
+    resetMarkup;
     if (textInput === '') {
         return
     };
     API.fetchPictures(textInput)
         .then(renderMarkup)
         .catch(onFetchError)
+    
+    e.target.searchQuery.value = '';
 };
 
 function resetMarkup() {
-    console.log('будет убивать разметку')
+    refs.galleryContainer.innerHTML = '';
 };
 
 function renderMarkup(pictures) {
-    // const createdEl = pictures.map
-    console.log(pictures);
+    
+    if (pictures.hits.length === 0) {
+        Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');   
+    };
+    
+    const createdElements = pictures.hits.map(el => {
+        const createdEl = `
+        <div class="photo-card">
+            <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                <b>Likes: ${el.likes}</b>
+                </p>
+                <p class="info-item">
+                <b>Views: ${el.views}</b>
+                </p>
+                <p class="info-item">
+                <b>Comments: ${el.comments}</b>
+                </p>
+                <p class="info-item">
+                <b>Downloads: ${el.downloads}</b>
+                </p>
+            </div>
+            </div>
+        `;
+        return createdEl;
+    }).join('');
 
+    refs.galleryContainer.innerHTML = createdElements;
 };
 
-function onFetchError() {'будет выдавать ошибки'};
+function onFetchError() {
+    Notiflix.Notify.failure('WTF?!');
+};
