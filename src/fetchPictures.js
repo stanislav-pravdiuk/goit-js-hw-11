4449eaa3862baab81f6b7f8c90e35e33baf63a14
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-03-22 11:52:48 +0200

    save add axios

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,16 +1,16 @@
-const API_KEY = '34574978-aeefc0d62f6da3cbea3bfb7cd';
-let page = 0;
-const per_page = 40;
+// const API_KEY = '34574978-aeefc0d62f6da3cbea3bfb7cd';
+// let page = 0;
+// const per_page = 40;
 
-function fetchPictures(searchQuery) {
-    page += 1;
-    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`;
-    return fetch(url).then(response => {
-        if (response.ok) {
-            return response.json()
-        } throw new Error(response.statusText)
-    } 
-) 
-};
+// function fetchPictures(searchQuery) {
+//     page += 1;
+//     const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`;
+//     return fetch(url).then(response => {
+//         if (response.ok) {
+//             return response.json()
+//         } throw new Error(response.statusText)
+//     } 
+// ) 
+// };
 
-export default {fetchPictures} 
+// export default {fetchPictures} 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import API from './fetchPictures';
+// import API from './fetchPictures';
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
@@ -11,6 +12,11 @@ refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 let searchQuery = '';
+let totalHits = 1000;
+const API_KEY = '34574978-aeefc0d62f6da3cbea3bfb7cd';
+let page = 0;
+const per_page = 40;
+
 
 function onSearch(e) {
     e.preventDefault();
@@ -20,9 +26,9 @@ function onSearch(e) {
         location. reload()
         return
     };
-    API.fetchPictures(searchQuery)
-        .then(renderMarkup)
-        .catch(onFetchError)
+    fetchPictures(searchQuery)
+        // .then(renderMarkup)
+        // .catch(onFetchError)
     
     e.target.searchQuery.value = '';
 };
@@ -32,6 +38,7 @@ function resetMarkup() {
 };
 
 function renderMarkup(pictures) {
+    totalHits = pictures.totalHits
     if (pictures.hits.length === 0) {
         Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');   
     };
@@ -67,7 +74,40 @@ function onFetchError() {
 };
 
 function onLoadMore() {
-    API.fetchPictures(searchQuery)
+    fetchPictures(searchQuery)
         .then(renderMarkup)
         .catch(onFetchError)
+};
+
+function fetchPictures(searchQuery) {
+    page += 1;
+    console.log(totalHits)
+    if ((totalHits - page * per_page) < 0) {
+        Notiflix.Notify.warning('We`re sorry, but you`ve reached the end of search results.');
+        return
+    }
+    // const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`;
+
+    axios({
+    method: 'get',
+    url: 'https://pixabay.com/api/',
+    params: {
+        key: '34574978-aeefc0d62f6da3cbea3bfb7cd',
+        q: searchQuery,
+        per_page: per_page,
+        page: page,
+        safesearch: true,
+        orientation: 'horizontal',
+        image_type: 'photo',
+  }
+    })
+    
+        .then(renderMarkup)
+        .catch(onFetchError)
+    // return fetch(url).then(response => {
+    //     if (response.ok) {
+    //         return response.json()
+    //     } throw new Error(response.statusText)
+    // } 
+// ) 
 };
